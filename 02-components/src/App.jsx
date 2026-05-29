2d0732c894f966ee081f53a2e726f77385dea376
jatinukey2 <jatinukey2@gmail.com>
2026-05-29 16:56:21 +0530

    add components folder

---
+++ 02-components/src/App.jsx
@@ -0,0 +1,12 @@
+import Card from "./components/Card.jsx";
+import Navbar from "./components/Navbar.jsx"
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Card />
+    </div>
+  )
+}
+
+export default App
---
+++ 02-components/src/components/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>
+      <div className="card">
+        <h1>Jatin Ukey</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, reprehenderit!</p>
+      </div>;
+    </div>
+  )
+}
+
+export default Card
