8c3bca1a1621e77a5ff962e381a9c0fab50b352a
jatinukey2 <jatinukey2@gmail.com>
2026-05-29 12:17:10 +0530

    add 01-folder

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return <>
+    <div id='dad'>
+      <h1 id='child1'>Hello there</h1>
+      <h2 id='child2'>How are you</h2>
+    </div>
+    <div id='uncle'></div>
+  </>
+}
+
+export default App
